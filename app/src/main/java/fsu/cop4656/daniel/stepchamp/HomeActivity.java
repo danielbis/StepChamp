8f4952b5ecdf2333f59b22ab55bc513d1957bed4
danielbis <dmb16f@my.fsu.edu>
2018-11-11 21:38:10 -0500

    Foreground stepSensor added, not tested because of the emulator limitations.

--- app/src/main/java/fsu/cop4656/daniel/stepchamp/HomeActivity.java
+++ app/src/main/java/fsu/cop4656/daniel/stepchamp/HomeActivity.java
@@ -1,9 +1,15 @@
 package fsu.cop4656.daniel.stepchamp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -14,13 +20,34 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class HomeActivity extends AppCompatActivity {
+import static android.app.Service.START_STICKY;
+
+public class HomeActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mStepCounterSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
+
+    public void onSensorChanged(SensorEvent event){
+        float[] values = event.values;
+        Log.d("onSensorChanged", "value: " + String.valueOf(values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d("onSensorAccuracyChanged", "value: " + String.valueOf(accuracy));
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
