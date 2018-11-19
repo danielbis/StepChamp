21e434f7170c16a4de3fa526bf9e45497cf5f5ad
NoHashNoCash <42846208+NoHashNoCash@users.noreply.github.com>
2018-11-18 20:37:08 -0500

    Add files via upload

--- app/src/main/java/fsu/cop4656/daniel/stepchamp/FirebaseUIActivity.java
+++ app/src/main/java/fsu/cop4656/daniel/stepchamp/FirebaseUIActivity.java
@@ -1,10 +1,23 @@
 package fsu.cop4656.daniel.stepchamp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import android.support.design.widget.Snackbar;
 import com.firebase.ui.auth.AuthUI;
@@ -14,19 +27,45 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FirebaseUIActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 123;
     private static final String TAG = "FirebaseUIActivity";
+    private LocationManager mLocationManager;
+    private boolean mLocationPermissionGranted = false;
+    private double mLongitude = 0;
+    private double mLatitude = 0;
+
+    private static final int PERMISSION_REQUEST_LOCATIONS = 3;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_ui);
+        mLocationManager = (LocationManager) getSystemService(
+                LOCATION_SERVICE);
         authenticate(); // start sign-in process
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationManager.removeUpdates(mLocationListener);
     }
 
     public void authenticate() {
@@ -44,7 +83,7 @@ public class FirebaseUIActivity extends AppCompatActivity {
         // [END check_current_user]
     }
 
-    public void createSignInIntent(){
+    public void createSignInIntent() {
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.GoogleBuilder().build(),
@@ -72,6 +111,7 @@ public class FirebaseUIActivity extends AppCompatActivity {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Intent intent = new Intent(this, HomeActivity.class);
+                saveToDatabase(user);
                 startActivity(intent);
                 // ...
             } else {
@@ -92,8 +132,177 @@ public class FirebaseUIActivity extends AppCompatActivity {
 
             }
         }
+
+
+
+
+    }
+    // Location code below
+    public void requestLocationUpdates(View v) {
+        Log.i(TAG, "Requesting location...");
+
+        if(!mLocationPermissionGranted) {
+            requestLocationsPermission();
+        }
+        try {
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    0, 0, mLocationListener);
+
+        } catch (SecurityException e) {
+            requestLocationsPermission();
+        }
+    }
+
+    private void requestLocationsPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(FirebaseUIActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(FirebaseUIActivity.this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(FirebaseUIActivity.this);
+            builder.setTitle("Requesting internet permissions");
+            builder.setMessage("This application requires internet. Accept to continue");
+            builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCompat.requestPermissions(FirebaseUIActivity.this,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                                    android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                            PERMISSION_REQUEST_LOCATIONS);
+                }
+            });
+            builder.show();
+        } else {
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(FirebaseUIActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSION_REQUEST_LOCATIONS);
+        }
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_LOCATIONS) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Location permissions were granted.");
+            } else {
+                // Permission request was denied.
+                Toast.makeText(FirebaseUIActivity.this,
+                        "Location permission request was denied. Location permission is required for this application ",
+                        Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Location permission request was denied. Requesting again...");
+                requestLocationsPermission(); // repeat until they confirm
+            }
+        }
+    }
 
+
+    private void setLastKnownLocation() {
+        if (!mLocationPermissionGranted) {
+            requestLocationsPermission();
+        }
+        Location lastLoc = null;
+        try {
+            lastLoc = mLocationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        } catch (SecurityException e) {
+            requestLocationsPermission();
+        }
+        if(lastLoc == null) {
+            Log.e(TAG, "No last known location"); // log to errors
+            return;
+        }
+
+
+        mLatitude = lastLoc.getLatitude();
+        mLongitude = lastLoc.getLongitude();
+
+        if (mLatitude == 0|| mLongitude == 0) {
+           Log.e(TAG, "Location not set!");
+        }
+    }
+
+
+    private LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Waiting for location",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(getApplicationContext(), "Connection Lost",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Location lastLoc = null;
+
+            try {
+                lastLoc = mLocationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            } catch (SecurityException e) {
+                requestLocationsPermission();
+            }
+            mLatitude = lastLoc.getLatitude();
+            mLongitude = lastLoc.getLongitude();
+
+            if (mLatitude == 0|| mLongitude == 0) {
+                Log.e(TAG, "Location not changed!");
+            }
+        }
+    };
+
+    public boolean saveToDatabase(FirebaseUser acct) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("users");
+
+
+        setLastKnownLocation(); // get the location 
+        User user = new User(acct.getDisplayName(), Double.toString(mLatitude), Double.toString(mLongitude), 125);
+
+        String key = myRef.push().getKey();
+        Map<String, Object> postValues = user.toMap();
+        final Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, postValues);
+
+
+        Query query = FirebaseDatabase.getInstance().getReference("users")
+                .orderByChild("nickname").equalTo(acct.getDisplayName());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+              if(!dataSnapshot.exists()){
+                  myRef.updateChildren(childUpdates);
+              }
+              else{
+                  return;
+              }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("FAIL", "Failed to read value.", error.toException());
+            }
+        });
+        return true;
+    }
 }
+
+
+
+
+
+
+
--- app/src/main/java/fsu/cop4656/daniel/stepchamp/HomeActivity.java
+++ app/src/main/java/fsu/cop4656/daniel/stepchamp/HomeActivity.java
@@ -7,43 +7,197 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.common.util.ArrayUtils;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static android.app.Service.START_STICKY;
+import static java.lang.Math.toIntExact;
 
-public class HomeActivity extends AppCompatActivity implements SensorEventListener {
+
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private SensorManager mSensorManager;
     private Sensor mStepCounterSensor;
+
+    private GoogleMap mMap;
+    Marker marker0;
+    Marker marker1;
+    Marker marker2;
+    Marker marker3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+       /* mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);*/
 
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng FSU = new LatLng(30.441210, -84.298050);
+
+
+        Query query = FirebaseDatabase.getInstance().getReference("users")
+                .orderByChild("totalsteps")
+                .limitToLast(3);
+        // https://stackoverflow.com/questions/30933328/how-to-convert-firebase-data-to-java-object
+        final Double[] latitudearray = new Double[4];
+        final Double[] longitudearray = new Double[4];
+        final int[] steparray = new int[4];
+
+
+
+        final String[] namearray = new String[4];
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+                for(DataSnapshot messageSnapshot : dataSnapshot.getChildren()) {
+
+                    String latitude = (String) messageSnapshot.child("latitude").getValue();
+                    Double latval = Double.parseDouble(latitude);
+                    latitudearray[i] = latval;
+
+                    String longitude = (String) messageSnapshot.child("longitude").getValue();
+                    Double longval = Double.parseDouble(longitude);
+                    longitudearray[i] = longval;
+                    namearray[i] = (String) messageSnapshot.child("nickname").getValue();
+
+                    int steps = (int)(long) messageSnapshot.child("totalsteps").getValue();
+                    int stepval = steps;
+                    steparray[i] = stepval;
+
+                    i++;
+
+                    if( i == 3) {
+
+                    break;
+                    }
+                }
+                Log.d("LAT", "value = " + i );
+                for(int j = i-1; j >-1; j--) {
+
+                    Log.i("NAME",namearray[j]);
+                    Log.d("LAT", "value = " + latitudearray[j]);
+                    Log.d("LONG", "value = " + longitudearray[j]);
+                    if(j==0){
+                        marker0 = mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(latitudearray[j], longitudearray[j]))
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                            .title(namearray[j])
+                            .snippet( "Steps: " + steparray[j]));
+                   }
+                    if(j==1){
+                        marker1 = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(latitudearray[j], longitudearray[j]))
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                                .title(namearray[j])
+                                .snippet( "Steps: " + steparray[j]));
+                    }
+                    if(j==2){
+                       marker2 = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(latitudearray[j], longitudearray[j]))
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW))
+                                .title(namearray[j])
+                                .snippet( "Steps: " + steparray[j]));
+                    }
+                    if(j==3){
+                        marker3 = mMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(latitudearray[j], longitudearray[j]))
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+                                .title(namearray[j])
+                                .snippet( "Steps: " + steparray[j]));
+                    }
+
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("FAIL", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(FSU,(float)15.25));
+    }
+
+    public void SelectMarker(int position){
+        Log.i("POSITION:","POS:" + position);
+
+        if(position == 0){
+            marker2.showInfoWindow();
+        }
+        if(position == 1){
+            marker1.showInfoWindow();
+        }
+        if(position == 2){
+            marker0.showInfoWindow();
+        }
+
+        return;
+    }
+
+
+
 
     public void onSensorChanged(SensorEvent event){
         float[] values = event.values;
         Log.d("onSensorChanged", "value: " + String.valueOf(values[0]));
     }
 
-    @Override
+
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.d("onSensorAccuracyChanged", "value: " + String.valueOf(accuracy));
     }
